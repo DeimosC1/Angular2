5b2e5fb86894c29909f0296bba5fa89e43598a9f
DeimosC1 <62855417+DeimosC1@users.noreply.github.com>
2021-01-03 18:10:31 +0100

    commit

--- src/app/blog-post/blog-post.component.spec.ts
+++ src/app/blog-post/blog-post.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BlogPostComponent } from './blog-post.component';
+import { BlogComponent } from './blog-post.component';
 
-describe('BlogPostComponent', () => {
-  let component: BlogPostComponent;
-  let fixture: ComponentFixture<BlogPostComponent>;
+describe('BlogComponent', () => {
+  let component: BlogComponent;
+  let fixture: ComponentFixture<BlogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BlogPostComponent ]
+      declarations: [ BlogComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BlogPostComponent);
+    fixture = TestBed.createComponent(BlogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss']
 })
-export class BlogPostComponent implements OnInit {
-  constructor() { }
+export class BlogComponent implements OnInit {
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  postBlog(header, body, numer) {
+    if (numer < 0 || numer == null) {
+      let outputPlace = document.getElementById("BlogPost");
+      let obal = document.createElement("div");
+      let headrelem = document.createElement("h3");
+      headrelem.innerText = header;
+      obal.appendChild(headrelem);
+      let bodyelem = document.createElement("p");
+      bodyelem.innerText = body;
+      obal.appendChild(bodyelem);
+      outputPlace.appendChild(obal);
+    } else {
+      if (header == "" && body == "") {
+        document.getElementById("BlogPost").removeChild(numer);
+      } else {
+        let outputPlace = document.getElementById("BlogPost").childNodes[numer];
+        outputPlace.childNodes[0].textContent = header;
+        outputPlace.childNodes[1].textContent = body;
+      }
+    }
+
+  }
 }
