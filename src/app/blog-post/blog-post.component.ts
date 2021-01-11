88efe08b42a8c939b290be0138f32bab649c62e0
DeimosC1 <62855417+DeimosC1@users.noreply.github.com>
2021-01-11 16:09:12 +0100

    Update blog-post.component.ts

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -1,40 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 
+interface IBlogPost {
+  title: string;
+  body:string;
+  showForm:boolean;
+
+}
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss']
 })
-export class BlogComponent implements OnInit {
+export class BlogPostComponent{
+title = 'why';
 
-  constructor() {
-  }
+posts: IBlogPost[] = [
+  {title:'blog1', body:'why',showForm:false},
+  {title:'blog2', body:'why',showForm:false}
 
-  ngOnInit(): void {
 
-  }
+];
 
-  postBlog(header, body, numer) {
-    if (numer < 0 || numer == null) {
-      let outputPlace = document.getElementById("BlogPost");
-      let obal = document.createElement("div");
-      let headrelem = document.createElement("h3");
-      headrelem.innerText = header;
-      obal.appendChild(headrelem);
-      let bodyelem = document.createElement("p");
-      bodyelem.innerText = body;
-      obal.appendChild(bodyelem);
-      outputPlace.appendChild(obal);
-    } else {
-      if (header == "" && body == "") {
-        document.getElementById("BlogPost").removeChild(numer);
-      } else {
-        let outputPlace = document.getElementById("BlogPost").childNodes[numer];
-        outputPlace.childNodes[0].textContent = header;
-        outputPlace.childNodes[1].textContent = body;
-      }
-    }
 
-  }
 }
+
